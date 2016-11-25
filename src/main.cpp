478ef7d09653092987d32cdfb6910325bf672427
Zoltan Onody <zoltan.onody@gmail.com>
2016-11-25 23:50:51 +0100

    Add CLion support

---
+++ src/main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main(){
+    std::cout << "It works like charm!" << std::endl;
+    return 0;
+}
---
+++ src/riesenie.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+#include "riesenie.h"
+
+using namespace std;
+
+const bool DUMMY_BOOL = false;
+const int DUMMY_INT = -0;
+const string DUMMY_STRING = "";
+
+// Uloha 1.
+
+
+int ZString::length(){
+	return DUMMY_INT;
+}
