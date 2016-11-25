228faa106b79729d52e44894621bffcac9f6e638
Zoltan Onody <zoltan.onody@gmail.com>
2016-11-25 22:50:27 +0100

    initial commit for project structure

---
+++ riesenie.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <sstream>
+#include <iomanip>
+
+#include "riesenie.h"
+
+using namespace std;
+
+const bool DUMMY_BOOL = false;
+const int DUMMY_INT = -47;
+const string DUMMY_STRING = "";
+
+int ZString::length(){
+	return DUMMY_INT;
+}
---
+++ riesenie.h
@@ -0,0 +1,12 @@
+#if !defined( _RIESENIE_H_ )
+#define _RIESENIE_H_
+
+class ZString {
+	int len;
+	char *data;
+
+public:
+	int length();
+};
+
+#endif
