3a94fc620185430a6c2d6d94911a4426214f3a41
Zoltan Onody <zoltan.onody@gmail.com>
2016-11-26 12:53:53 +0100

    Fix CRLF

--- cmake-build-debug/CMakeFiles/semestral_project.dir/cmake_clean.cmake
+++
@@ -1,5 +0,0 @@
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/semestral_project.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- cmake-build-debug/test/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/zoli/AIN/programming3/semestral_project/test
-# Build directory: /home/zoli/AIN/programming3/semestral_project/cmake-build-debug/test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(../googletest-build)
